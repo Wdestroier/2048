34df907906353bb0c545132a5b2e401127eb8582
Wdestroier <Wdestroier@gmail.com>
2021-09-26 00:48:02 -0300

    Change grid squares sum order

--- src/game_grid_movement.c
+++ src/game_grid_movement.c
@@ -10,27 +10,28 @@ bool grid_move_up()
 	{
 		for (int c = 0; c < GRID_SIDE_SQUARE_COUNT; c++)
 		{
-			// Ignore the first square (0), because it isn't added or moved to the prev square (-1)
-			for (int r = GRID_SIDE_SQUARE_COUNT - 1; r > 0; r--)
+			// Ignore the last square (GRID_SIDE_SQUARE_COUNT - 1), because the
+			// next value (GRID_SIDE_SQUARE_COUNT) doesn't exist
+			for (int r = 0; r < GRID_SIDE_SQUARE_COUNT - 1; r++)
 			{
 				int* current_square_value = &game_grid[r][c];
-				int* prev_square_value = &game_grid[r - 1][c];
+				int* next_square_value = &game_grid[r + 1][c];
 
-				if (*current_square_value == 0)
+				if (*next_square_value == 0)
 				{
 					continue;
 				}
-				else if (*current_square_value == *prev_square_value)
+				else if (*next_square_value == *current_square_value)
 				{
-					*prev_square_value += *current_square_value;
-					*current_square_value = 0;
+					*current_square_value += *next_square_value;
+					*next_square_value = 0;
 
 					moved = true;
 				}
-				else if (*prev_square_value == 0)
+				else if (*current_square_value == 0)
 				{
-					*prev_square_value = *current_square_value;
-					*current_square_value = 0;
+					*current_square_value = *next_square_value;
+					*next_square_value = 0;
 
 					moved = true;
 				}
@@ -49,28 +50,27 @@ bool grid_move_down()
 	{
 		for (int c = 0; c < GRID_SIDE_SQUARE_COUNT; c++)
 		{
-			// Ignore the last square (GRID_SIDE_SQUARE_COUNT - 1), because it
-			// isn't added or moved to the next square (GRID_SIDE_SQUARE_COUNT)
-			for (int r = 0; r < GRID_SIDE_SQUARE_COUNT - 1; r++)
+			// Ignore the last square (0), because the prev value (-1) doesn't exist
+			for (int r = GRID_SIDE_SQUARE_COUNT - 1; r > 0; r--)
 			{
 				int* current_square_value = &game_grid[r][c];
-				int* next_square_value = &game_grid[r + 1][c];
+				int* prev_square_value = &game_grid[r - 1][c];
 
-				if (*current_square_value == 0)
+				if (*prev_square_value == 0)
 				{
 					continue;
 				}
-				else if (*current_square_value == *next_square_value)
+				else if (*prev_square_value == *current_square_value)
 				{
-					*next_square_value += *current_square_value;
-					*current_square_value = 0;
+					*current_square_value += *prev_square_value;
+					*prev_square_value = 0;
 
 					moved = true;
 				}
-				else if (*next_square_value == 0)
+				else if (*current_square_value == 0)
 				{
-					*next_square_value = *current_square_value;
-					*current_square_value = 0;
+					*current_square_value = *prev_square_value;
+					*prev_square_value = 0;
 
 					moved = true;
 				}
@@ -91,27 +91,28 @@ bool grid_move_left()
 		{
 			int* current_row = game_grid[r];
 
-			// Ignore the first square (0), because it isn't added or moved to the prev square (-1)
-			for (int c = GRID_SIDE_SQUARE_COUNT - 1; c > 0; c--)
+			// Ignore the last square (GRID_SIDE_SQUARE_COUNT - 1), because the
+			// next value (GRID_SIDE_SQUARE_COUNT) doesn't exist
+			for (int c = 0; c < GRID_SIDE_SQUARE_COUNT - 1; c++)
 			{
 				int* current_square_value = &current_row[c];
-				int* prev_square_value = &current_row[c - 1];
+				int* next_square_value = &current_row[c + 1];
 
-				if (*current_square_value == 0)
+				if (*next_square_value == 0)
 				{
 					continue;
 				}
-				else if (*current_square_value == *prev_square_value)
+				else if (*next_square_value == *current_square_value)
 				{
-					*prev_square_value += *current_square_value;
-					*current_square_value = 0;
+					*current_square_value += *next_square_value;
+					*next_square_value = 0;
 
 					moved = true;
 				}
-				else if (*prev_square_value == 0)
+				else if (*current_square_value == 0)
 				{
-					*prev_square_value = *current_square_value;
-					*current_square_value = 0;
+					*current_square_value = *next_square_value;
+					*next_square_value = 0;
 
 					moved = true;
 				}
@@ -132,28 +133,27 @@ bool grid_move_right()
 		{
 			int* current_row = game_grid[r];
 
-			// Ignore the last square (GRID_SIDE_SQUARE_COUNT - 1), because it
-			// isn't added or moved to the next square (GRID_SIDE_SQUARE_COUNT)
-			for (int c = 0; c < GRID_SIDE_SQUARE_COUNT - 1; c++)
+			// Ignore the last square (0), because the previous value (-1) doesn't exist
+			for (int c = GRID_SIDE_SQUARE_COUNT - 1; c > 0; c--)
 			{
 				int* current_square_value = &current_row[c];
-				int* next_square_value = &current_row[c + 1];
+				int* prev_square_value = &current_row[c - 1];
 
-				if (*current_square_value == 0)
+				if (*prev_square_value == 0)
 				{
 					continue;
 				}
-				else if (*current_square_value == *next_square_value)
+				else if (*prev_square_value == *current_square_value)
 				{
-					*next_square_value += *current_square_value;
-					*current_square_value = 0;
+					*current_square_value += *prev_square_value;
+					*prev_square_value = 0;
 
 					moved = true;
 				}
-				else if (*next_square_value == 0)
+				else if (*current_square_value == 0)
 				{
-					*next_square_value = *current_square_value;
-					*current_square_value = 0;
+					*current_square_value = *prev_square_value;
+					*prev_square_value = 0;
 
 					moved = true;
 				}
